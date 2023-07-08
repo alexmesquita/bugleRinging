3c2983b6ee2bc6898939118242ba73d7a849def3
Álex Mesquita <alex.mesquita0608@gmail.com>
2023-07-08 15:35:11 -0300

    adding expo-av

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Pressable } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import {Audio} from 'expo-av'
 
 import { RingingList } from './src/screens/RingingList'
 
 export default function App() {
-  return <View style={styles.container}></View>
+  const [audioFileUri, setAudioFileUri] = useState<string | null>(
+    './assets/audio/two.mp3',
+  )
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <MaterialIcons name="play-arrow" size={44} color="#F2F" />
+      </Pressable>
+    </View>
+  )
 
   // return <RingingList />
 }
