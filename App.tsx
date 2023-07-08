44acf64b3bd5afaa2e42956983e848dbc096a17c
Álex Mesquita <alex.mesquita0608@gmail.com>
2023-07-08 15:17:19 -0300

    testing a new lib

--- App.tsx
+++ App.tsx
@@ -1,54 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-} from 'react-native'
-
-import TrackPlayer from 'react-native-track-player'
-import {
-  playbackService,
-  setupPlayer,
-  addTrack,
-} from './src/services/musicPlayerServices'
+import { StyleSheet, View } from 'react-native'
 
 import { RingingList } from './src/screens/RingingList'
-import MusicPlayer from './src/screens/MusicPlayer'
 
 export default function App() {
-  const [isPlayerReady, setIsPaylerReady] = useState(false)
-
-  async function setup() {
-    const isSetup = await setupPlayer()
-
-    if (isSetup) {
-      await addTrack()
-    }
-
-    setIsPaylerReady(isSetup)
-  }
-
-  useEffect(() => {
-    setup()
-  }, [])
-
-  if (!isPlayerReady) {
-    return (
-      <SafeAreaView>
-        <ActivityIndicator />
-      </SafeAreaView>
-    )
-  }
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={'light-content'} />
-      <MusicPlayer />
-    </View>
-  )
+  return <View style={styles.container}></View>
 
   // return <RingingList />
 }
@@ -58,5 +15,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 })
-
-TrackPlayer.registerPlaybackService(() => playbackService)
