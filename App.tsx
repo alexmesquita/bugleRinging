b8c3bbfeb2a18286486861d762d28960808c5604
Álex Mesquita <alex.mesquita0608@gmail.com>
2023-07-10 11:02:47 -0300

    playing audio

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { StyleSheet, View, Pressable } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
-import {Audio} from 'expo-av'
+import { Audio } from 'expo-av'
 
 import { RingingList } from './src/screens/RingingList'
 
@@ -10,10 +10,31 @@ export default function App() {
   const [audioFileUri, setAudioFileUri] = useState<string | null>(
     './assets/audio/two.mp3',
   )
+
+  async function handleAudioPlay() {
+    console.log('ENTROU')
+    const { sound } = await Audio.Sound.createAsync(
+      {
+        uri: 'https://samplelib.com/lib/preview/mp3/sample-3s.mp3',
+      },
+      { shouldPlay: true },
+    )
+    console.log('carregou')
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
+  useEffect(() => {
+    Audio.setAudioModeAsync({ playsInSilentModeIOS: true })
+  })
   return (
     <View style={styles.container}>
-      <Pressable>
-        <MaterialIcons name="play-arrow" size={44} color="#F2F" />
+      <Pressable style={styles.button}>
+        <MaterialIcons
+          name="play-arrow"
+          size={44}
+          color="#F2F"
+          onPress={handleAudioPlay}
+        />
       </Pressable>
     </View>
   )
@@ -24,5 +45,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  button: {
+    paddingTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
